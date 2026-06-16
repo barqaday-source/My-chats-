8028257c6f476de7d7de42ef8ab291dda6cffaa9
barq <barqaday@gmail.com>
2026-06-16 19:49:41 +0300

    Update Supabase URL and anonKey in configuration

--- lib/core/constants/supabase_config.dart
+++ lib/core/constants/supabase_config.dart
@@ -2,9 +2,9 @@ import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:flutter/foundation.dart';
 
 class SupabaseConfig {
-  static const url = 'https://jmsmrojtlstppnpwmkkk.supabase.co';
+  static const url = 'https://vohlleqcuomudoryiwkc.supabase.co';
   static const anonKey =
-      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imptc21yb2p0bHN0cHBucHdta2trIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI4MTg2NDAsImV4cCI6MjA4ODM5NDY0MH0.j7gxr5CvrfvbJJzK_pMwVHiCE2AqpXUTThpeLEBmsos';
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZvaGxsZXFjdW9tdWRvcnlpd2tjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE2MjE3NzAsImV4cCI6MjA5NzE5Nzc3MH0.VNUs7_WXzAeSz5TC_aD56FfzFQkmc_p99PY_b7hPZYU';
 
   // Tables
   static const tUsers = 'users';
