8906479657f38198460048970e77fb89d0e2996d
barq <barqaday@gmail.com>
2026-06-17 23:24:09 +0300

    Update supabase_config.dart

--- lib/core/constants/supabase_config.dart
+++ lib/core/constants/supabase_config.dart
@@ -2,7 +2,7 @@ import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:flutter/foundation.dart';
 
 class SupabaseConfig {
-  static const url = 'https://jmsmrojtlstppnpwmkkk.supabase.co';
+  static const url = 'https://vohlleqcuomudoryiwkc.supabase.co';
   static const anonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZvaGxsZXFjdW9tdWRvcnlpd2tjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE2MjE3NzAsImV4cCI6MjA5NzE5Nzc3MH0.VNUs7_WXzAeSz5TC_aD56FfzFQkmc_p99PY_b7hPZYU';
 
   // Tables - موحد
