dd65366fda2cb14113833f6f6e6a0cc76c067bfe
barq <barqaday@gmail.com>
2026-06-17 16:51:41 +0300

    Update supabase_config.dart

--- lib/core/constants/supabase_config.dart
+++ lib/core/constants/supabase_config.dart
@@ -6,27 +6,6 @@ class SupabaseConfig {
   static const anonKey =
       'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZvaGxsZXFjdW9tdWRvcnlpd2tjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE2MjE3NzAsImV4cCI6MjA5NzE5Nzc3MH0.VNUs7_WXzAeSz5TC_aD56FfzFQkmc_p99PY_b7hPZYU';
 
-  // Tables
-  static const tUsers = 'users';
-  static const tRooms = 'rooms';
-  static const tMessages = 'messages';
-  static const tPrivateMessages = 'private_messages'; // ضيف هذا
-  static const tRoomMessages = 'room_messages'; // ضيف هذا
-  static const tNotifications = 'notifications';
-  static const tReports = 'reports';
-  static const tRoomMembers = 'room_members';
-  static const tPrivateChats = 'private_chats';
-  static const tBlockedUsers = 'blocked_users'; 
-  static const tContactInfo = 'contact_info';
-  static const tBlocks = 'blocks';
-  static const tAdmins = 'admins'; // ضيف هذا
-
-  // Storage buckets - محدثة للأسماء الجديدة
-  static const bucketAvatars = 'avatars';
-  static const bucketRooms = 'room-images'; 
-  static const bucketMedia = 'chat_images'; // كان chat-media
-  static const bucketAudio = 'voice_messages'; // كان audio-messages
-
   static bool _initialized = false;
 
   static Future<bool> init() async {
